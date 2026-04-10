5e5838475f04813e77f8d7700a8480ce8b8b90b9
Noor-suhana <noorsuhana2317@gmail.com>
2026-04-10 10:26:17 +0530

    Add files via upload

---
+++ inserting node at beginning in single linked list.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main()
+{
+    struct node *head = 0, *temp, *newnode, *newnode2;
+    int i, n;
+
+    printf("\nEnter n size: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("\nEnter node %d data: ", i + 1);
+        scanf("%d", &newnode->data);
+
+        newnode->link = 0;
+
+        if (head == 0)
+            head = temp = newnode;
+        else
+        {
+            temp->link = newnode;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+    while (temp != 0)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->link;
+    }
+    printf("NULL");
+
+    newnode2 = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter new node data: ");
+    scanf("%d", &newnode2->data);
+
+    newnode2->link = head;
+    head = newnode2;
+
+    temp = head;
+    while (temp != 0)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->link;
+    }
+    printf("NULL");
+return 0;
+}
